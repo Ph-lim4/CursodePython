563d173cfd86f4ef66611846ec1c84842506b98a
ph <limabjlapa@gmail.com>
2023-08-02 18:00:05 -0300

    ex072 up

--- ex072.py
+++ ex072.py
@@ -1,4 +1,23 @@
 #Numero por extenso
+cores = {
+    'limpa':'\033[m',
+    'vermelho':'\033[31m',
+    'verde':'\033[32m',
+    'amarelo':'\033[33m',
+    'azul':'\033[34m',
+    'lilas:':'\033[35m',
+    'azulc':'\033[36m',
+
+    'bordabranca':'\033[1:30:40m',
+    'bordavermelha': '\033[1:30:41m',
+    'bordaverde': '\033[1:30:42m',
+    'bordaamarela': '\033[1:30:43m',
+    'bordaazul': '\033[1:30:44m',
+    'bordalilas': '\033[1:30:45m',
+    'bordaazulc': '\033[1:30:46m',
+    'pretoebranco':'\033[7;30m',
+
+}
 
 cont = ('zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
         'Onze', 'Doze', 'Treze', 'Catorze', 'Quize', 'Dezesseis', 'Dezesete', 'Dezoito', 'Dezenove', 'Vinte')
@@ -6,7 +25,10 @@ while True:
     num = int(input('Digite um numero entre 0 e 20: '))
     extenso = ''
     if (num >= 0) and num <= 20:
-        break
+        print(f"Você digitou o número {cores['bordaverde']} {cont[num]} {cores['limpa']}")
+        resp = input('Deseja continuar? (S/N) ')
+        if resp in 'Nn':
+            break
     else:
         print('Número errado, digite novamente')
-print(f'Você digitou o número {cont[num]}')
+
