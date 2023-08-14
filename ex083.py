bd706f5dd182397174c3a52bb3c3e0b6dc6e9409
ph <limabjlapa@gmail.com>
2023-08-14 08:32:40 -0300

    ex083 up

---
+++ ex083.py
@@ -0,0 +1,16 @@
+# Analisando Expressões
+expr = str(input('Digire sua expressão: '))
+pilha = []
+
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+if len(pilha) == 0:
+    print('Sua expressão esta correta!')
+else:
+    print('Sua expressão está incorreta!')
