e2d4aad8dec42d5c598cf2df445afd264c0fcde9
ph <limabjlapa@gmail.com>
2023-09-05 09:52:23 -0300

    ex092 up

---
+++ ex092.py
@@ -0,0 +1,14 @@
+# Carteira de Trabalho
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dados['ctps'] != 0:
+    dados['anocontrato'] = int(input('Ano de contratação: '))
+    dados['salario'] = float(input('Salário: R$ '))
+    dados['aposentadoria'] = dados['idade'] + ((dados['anocontrato'] + 35) - datetime.now().year)
+# exibição dos dados
+for k, v in dados.items():
+    print(f'- {k} = {v}')
