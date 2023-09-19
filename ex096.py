c68e4be30fc17e37774a544d8d442313806505a7
ph <limabjlapa@gmail.com>
2023-09-19 18:54:14 -0300

    Aula 20

---
+++ ex096.py
@@ -0,0 +1,12 @@
+# area
+# Função
+def area(larg, comp):
+    print(f'A area do terreno {a}x{b} é igual a: {(a*b)}m²')
+
+
+# Programa principal com chamada para função
+print('Medindo área:')
+print('-'*20)
+a = float(input('Largura: '))
+b = float(input('Comprimento: '))
+area(a, b)
