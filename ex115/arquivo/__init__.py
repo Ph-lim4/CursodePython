fcee2d43b88e903254b9e6091e9a7498c08ef95f
Paulo Henrique Lima dos Santos <limabjlapa@gmail.com>
2024-05-21 13:26:24 +0200

    revisao e documentação 21/05.2

---
+++ ex115/arquivo/__init__.py
@@ -0,0 +1,67 @@
+from ex115.menu import *
+cores = {
+    'limpa':'\033[m',
+    'vermelho':'\033[31m',
+    'verde':'\033[32m',
+    'amarelo':'\033[33m',
+    'azul':'\033[34m',
+    'lilas:':'\033[35m',
+    'azulc':'\033[36m',
+
+    'bordabranca':'\033[1:30:40m',
+    'bordavermelha': '\033[1:30:41m',
+    'bordaverde': '\033[1:30:42m',
+    'bordaamarela': '\033[1:30:43m',
+    'bordaazul': '\033[1:30:44m',
+    'bordalilas': '\033[1:30:45m',
+    'bordaazulc': '\033[1:30:46m',
+    'pretoebranco':'\033[7;30m',
+
+}
+
+
+def arquivoExiste(arq):
+    try:
+        a = open(arq, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+
+def criararquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Hove um erro na criação do aqruivo!')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print(f'{cores["vermelho"]}Erro ao LER arquivo!{cores["limpa"]}')
+    else:
+        cabeçalho('PESSOAS CADASTRADAS')
+        print(a.read())
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print(f'{cores["vermelho"]}Erro ao ABRIR arquivo!{cores["limpa"]}')
+    else:
+        try:
+            a.write(f'{nome:<30}{idade:>5} anos\n')
+        except:
+            print(f'{cores["vermelho"]}Erro ao CRIAR Registro!{cores["limpa"]}')
+        else:
+            print(f'Novo Registro de {nome} adicionado.')
+            a.close()
--- ex115/main.py
+++ ex115/main.py
@@ -1,12 +1,22 @@
 from ex115.menu import *
+from ex115.arquivo import *
 
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criararquivo(arq)
 
 while True:
     resposta = menu()
     if resposta == 1:
-        print(resposta)
+        # READ - Leitura do Arquivo
+        lerArquivo(arq)
     elif resposta == 2:
-        print(resposta)
+        # CREAT - Cria um novo registro no Arquivo
+        cabeçalho('NOVO REGISTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
     else:
         cabeçalho('Saindo do sistema, até logo!')
         break
